b51358716f2358e60c4f05208f397e984cf1b2c2
Naga Chamala <naga@naga25d.cntrlsrvs.w2k.vt.edu>
2025-04-15 14:48:40 -0400

    added function to plot lines

--- postProcessVelData.py
+++ postProcessVelData.py
@@ -1,4 +1,3 @@
-import cv2
 import numpy as np
 import argparse
 import matplotlib.pyplot as plt
@@ -8,27 +7,44 @@ def plotVelAtPoint(path):
     mm_per_px = 0.02175955
     fps_capture = 130000
     factor = mm_per_px*1e-3*fps_capture
-    u1 = inputVelData[:,:,:,0]*factor
+    u = inputVelData[:,:,:,0]*factor
+    v = inputVelData[:,:,:,1]*factor
     #velMag = np.sqrt(u**2+v**2)
-    #pointX = 
     y = 44
     xPts = [20, 80, 140]
     yPts = [y,y,y]
     plt.figure(1)
     for x,y in zip(xPts,yPts):
-        #plt.imshow(u1[0,:,:])
-        #plt.plot(u1[:,y,x],'-',label='RAFT')
-        plt.plot(u1[:,y,x],'--',label='x = ' + str(x*mm_per_px) + ' mm')
+        plt.plot(u[:,y,x],'--',label='x = ' + str(x*8*mm_per_px) + ' mm')
         plt.legend()
     plt.xlabel('Frames')
     plt.ylabel('Velocity (m/s)')
     plt.savefig(path[:-4]+'_comparePts.png')
     plt.figure(2)
-    plt.imshow(u1[0,:,:])
+    plt.imshow(u[0,:,:])
     plt.scatter(xPts,yPts,c='red')
     plt.xlabel('x (px)')
     plt.ylabel('y (px)')
     #plt.show()
+
+def plotVelAtLines(path):
+    inputVelData = np.load(path)
+    mm_per_px = 0.02175955
+    fps_capture = 130000
+    factor = mm_per_px*1e-3*fps_capture
+    u = inputVelData[:,:,:,0]*factor
+    v = inputVelData[:,:,:,1]*factor
+    velMag = np.sqrt(u**2 + v**2)
+    xPts = [20, 80, 140]
+    print(np.mean(u[:,:,20],axis=0).shape)
+    plt.figure(1)
+    for x in xPts:
+        plt.plot(np.mean(u[:,:,x],axis=0),(u.shape[1]-1)*mm_per_px*8-np.arange(u.shape[1])*mm_per_px*8,label='x = ' + str(x*8*mm_per_px) + ' mm')
+        plt.legend()
+    plt.xlabel('Velocity (m/s)')
+    plt.ylabel('y (mm)')
+    plt.savefig(path[:-4]+'_compareLines.png')
+    
 
 def compareAtPoint(input1, input2):
     mm_per_px = 0.02175955
@@ -53,11 +69,16 @@ def compareAtPoint(input1, input2):
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('--path1', help="input video")
+    parser.add_argument('--method', help="type of plot")
+    parser.add_argument('--path', help="input video")
     parser.add_argument('--path2', help="input video")
     args = parser.parse_args()
-    #input1 = np.load(args.path1)
-    #input2 = np.load(args.path2)
-    plotVelAtPoint(args.path1)
-    #compareAtPoint(input1,input2)
+    if args.method == 'points':
+        plotVelAtPoint(args.path)
+    if args.method == 'lines':
+        plotVelAtLines(args.path)
+    elif args.method == 'compare':
+        input1 = np.load(args.path)
+        input2 = np.load(args.path2)
+        compareAtPoint(input1,input2)
     
