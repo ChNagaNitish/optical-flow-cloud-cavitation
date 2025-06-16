1d478bf4ad7d1e24ccb19d57f5d6319c91106284
Ch Naga Nitish <41124565+ChNagaNitish@users.noreply.github.com>
2025-06-16 12:32:44 -0400

    Update gpuJobScript.sh

--- gpuJobScript.sh
+++ gpuJobScript.sh
@@ -1,19 +1,20 @@
 #! /bin/bash
 #
-#SBATCH -t 0-10:00:00
+#SBATCH -t 0-3:00:00
 #SBATCH -N 1
 #SBATCH --account=cavitation
 #SBATCH --partition=a30_normal_q
-#SBATCH --gres=gpu:2
+#SBATCH --gres=gpu:1
 #SBATCH --mail-user=naga@vt.edu
 #SBATCH --mail-type=ALL
 #SBATCH --job-name=training
 
-# Loading required modules for OpenFOAM and Postprocessing
+# Loading required modules
 module purge
 module reset
 module load GCC/13.3.0 Python/3.12.3-GCCcore-13.3.0
 module load CUDA/12.6.0
+# Activating the Python Environment
 source "$HOME/workEnv/bin/activate"
-python -u train.py --name raft-cloudcavitation  --stage cloudcavitation --validation cloudcavitation --restore_ckpt checkpoints/raft-sintel.pth --gpus 0 1 --num_steps 50000 --batch_size 6 --lr 0.0001 --image_size 192 640 --wdecay 0.00001 --gamma=0.85
-#python evaluate.py --model=models/raft-sintel.pth --dataset=cloudcavitation
+# Running the Optical Flow code
+python tracking.py --method=raft --model=models/raft-sintel.pth --path=/home/naga/experiment/48.avi
