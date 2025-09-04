ac2f13d165dea39babd1d990d01f3b906def95cb
ChNagaNitish <chnaganitish@gmail.com>
2025-09-04 12:07:09 -0400

    saving in .h5 format with attributes

--- tracking.py
+++ tracking.py
@@ -22,7 +22,7 @@ def load_image(frame):
     img = torch.from_numpy(img).permute(2, 0, 1).float()
     return img[None].to(DEVICE)
 
-def raftOpticalFlow(args, inputVid,saveVel):
+def raftOpticalFlow(args, inputVid, window_height, window_width, roi, saveVel, mm_per_px, fps_capture):
     from raft import RAFT
     from utils.utils import InputPadder
     model = torch.nn.DataParallel(RAFT(args))
@@ -34,30 +34,53 @@ def raftOpticalFlow(args, inputVid,saveVel):
 
     with torch.no_grad():
         ret, prev_frame = inputVid.read()
+        if roi[-1]==-1:
+            if roi[1]==-1:
+                prev_gray = cv2.cvtColor(prev_frame, cv2.COLOR_BGR2GRAY)[roi[0]:,roi[2]:]
+            else:
+                prev_gray = cv2.cvtColor(prev_frame, cv2.COLOR_BGR2GRAY)[roi[0]:roi[1],roi[2]:]
+        else:
+            prev_gray = cv2.cvtColor(prev_frame, cv2.COLOR_BGR2GRAY)[roi[0]:roi[1],roi[2]:roi[3]]
         frame_count = int(inputVid.get(cv2.CAP_PROP_FRAME_COUNT))
         h, w = prev_frame.shape[:2]
-        window_size = [8,8]
-        window_height, window_width = window_size
         pad_height = window_height // 2
         pad_width = window_width // 2
-        kernel = np.ones(window_size) / (window_height*window_width)
-        velData = np.zeros((frame_count-1, h//window_height, w//window_width, 2), dtype='float64')
+        kernel = np.ones([window_height, window_width]) / (window_height*window_width)
+        data_shape = (frame_count-1, h//window_height, w//window_width, 2)
+        chunk_shape = (10, h//window_height, w//window_width, 2)
+        flow=None
+        f = h5py.File(saveVel, 'w')
+        velData = f.create_dataset('velocity',shape=data_shape,chunks=chunk_shape,dtype='float32')
         for frame_index in tqdm.tqdm(range(1,frame_count), desc="Processing Video Frame Pairs"):
             ret, curr_frame = inputVid.read()
+            if roi[-1]==-1:
+                if roi[1]==-1:
+                    curr_gray = cv2.cvtColor(curr_frame, cv2.COLOR_BGR2GRAY)[roi[0]:,roi[2]:]
+                else:
+                    curr_gray = cv2.cvtColor(curr_frame, cv2.COLOR_BGR2GRAY)[roi[0]:roi[1],roi[2]:]
+            else:
+                curr_gray = cv2.cvtColor(curr_frame, cv2.COLOR_BGR2GRAY)[roi[0]:roi[1],roi[2]:roi[3]]
             image1 = load_image(prev_frame)
             image2 = load_image(curr_frame)
             padder = InputPadder(image1.shape)
             image1, image2 = padder.pad(image1, image2)
             flow_low, flow_up = model(image1, image2, iters=20, test_mode=True)
             flow = flow_up[0].permute(1,2,0).cpu().numpy()
-            padded_arr = np.pad(flow, ((pad_height, pad_height), (pad_width, pad_width), (0, 0)), mode='reflect')
-            averaged_arr = np.zeros((h // window_height, w // window_width, 2), dtype=flow.dtype)
-            for channel in range(2):
-                averaged_arr[:, :, channel] = cv2.filter2D(padded_arr[:, :, channel], -1, kernel)[pad_height:-pad_height:window_height, pad_width:-pad_width:window_width]
-            velData[frame_index-1] = averaged_arr
+            if window_width!=1:
+                padded_arr = np.pad(flow, ((pad_height, pad_height), (pad_width, pad_width), (0, 0)), mode='reflect')
+                averaged_arr = np.zeros((h // window_height, w // window_width, 2), dtype=flow.dtype)
+                for channel in range(2):
+                    averaged_arr[:, :, channel] = cv2.filter2D(padded_arr[:, :, channel], -1, kernel)[pad_height:-pad_height:window_height, pad_width:-pad_width:window_width]
+                velData[frame_index-1,:,:,:] = averaged_arr
+            else:
+                velData[frame_index-1,:,:,:] = flow
             prev_frame = curr_frame
+        velData.attrs['window_height'] = window_height
+        velData.attrs['window_width'] = window_width
+        velData.attrs['mm_per_px'] = mm_per_px
+        velData.attrs['fps_capture'] = fps_capture
+        f.close()
         inputVid.release()
-        np.save(saveVel,np.array(velData))
     
 
 def farnebackMethod(inputVid, window_height, window_width, roi, saveVel, mm_per_px, fps_capture):
@@ -89,8 +112,6 @@ def farnebackMethod(inputVid, window_height, window_width, roi, saveVel, mm_per_
                 curr_gray = cv2.cvtColor(curr_frame, cv2.COLOR_BGR2GRAY)[roi[0]:roi[1],roi[2]:]
         else:
             curr_gray = cv2.cvtColor(curr_frame, cv2.COLOR_BGR2GRAY)[roi[0]:roi[1],roi[2]:roi[3]]
-        #plt.imshow(curr_gray)
-        #plt.show()
         flow = cv2.calcOpticalFlowFarneback(prev_gray, curr_gray, flow, 0.5, 3, 15, 3, 5, 1.2, 0)
         if window_width!=1:
             padded_arr = np.pad(flow, ((pad_height, pad_height), (pad_width, pad_width), (0, 0)), mode='reflect')
@@ -116,10 +137,10 @@ if __name__ == '__main__':
     parser.add_argument('--model', default='c1',help="raft-model or farneback-case or piv-case")
     parser.add_argument('--path', help="input video")
     parser.add_argument('--win_h', default=4, help="Averging window - height")
-    parser.add_argument('--win_h', default=4, help="Averging window - height")
-    parser.add_argument('--imgScale', default=0.001, help="Image Scale")
+    parser.add_argument('--win_w', default=4, help="Averging window - width")
+    parser.add_argument('--imgScale', default=0.001, help="Image Scale(mm/px)")
     parser.add_argument('--fpsCam', default=130000, help="FPS of camera used for capturing")
-    parser.add_argument('--roi', type=int, nargs=4, default=[0,-1,0,-1], help="Region of interest - hs he ws we")
+    parser.add_argument('--roi', type=int, nargs=4, default=[0,-1,0,-1], help="Region of interest in pixel indices - hs he ws we")
     parser.add_argument('--small', action='store_true', help='use small model')
     parser.add_argument('--mixed_precision', action='store_true', help='use mixed precision')
     parser.add_argument('--alternate_corr', action='store_true', help='use efficent correlation implementation')
@@ -133,7 +154,7 @@ if __name__ == '__main__':
         setattr(new_args, 'mixed_precision', args.mixed_precision)
         setattr(new_args, 'alternate_corr', args.alternate_corr)
         outputVelocityPath = args.path[:-4]+'_'+args.model.split("/")[-1][:-4]+'.npy'
-        raftOpticalFlow(new_args, inputVid, outputVelocityPath)
+        raftOpticalFlow(new_args, inputVid, int(args.win_h), int(args.win_w), args.roi, outputVelocityPath, float(args.imgScale), int(args.fpsCam))
     elif args.method == 'farneback':
         outputVelocityPath = args.path[:-4]+'_fb'+args.model+'.h5'
         farnebackMethod(inputVid, int(args.win_h), int(args.win_w), args.roi, outputVelocityPath, float(args.imgScale), int(args.fpsCam))
